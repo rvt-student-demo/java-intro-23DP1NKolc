f4e53ad4711bba89602664cffc9a353d24056428
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-09-24 09:45:16 +0000

    Sum of a sequence

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,25 +8,14 @@ public class App
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
-        int summa = 0;
-        int number = 0;
-        int sk = 0;
-        
-       while(true){
-        System.out.println("Give a number:");
-        number = Integer.valueOf(scanner.nextLine());
-        summa = number + summa;
-        sk+=1;
-
-        if(number==0){
-            break;
+        System.out.println("Last number? ");
+        int number = Integer.valueOf(scanner.nextLine());
+        int i=1;
+        int result=0;
+        while(i!=(number+1)){
+            result = result + i;
+            i++;
         }
-
-
-       }
-       System.out.println("Number of numbers: "+(sk-1));
-       System.out.println("Sum of the numbers: "+summa);
-
-       
+        System.out.println("The sum is: "+result); 
     }
 }
