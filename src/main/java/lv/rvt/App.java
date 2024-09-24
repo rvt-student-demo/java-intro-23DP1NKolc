c1f96a7d9097ca557615561d6049acc948f14dbe
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-09-24 07:51:18 +0000

    Number and sum of numbers

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -8,33 +8,25 @@ public class App
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
+        int summa = 0;
+        int number = 0;
+        int sk = 0;
         
-       
-       System.out.println("Value of the gift?");
-       int number1 = Integer.valueOf(scanner.nextLine());
+       while(true){
+        System.out.println("Give a number:");
+        number = Integer.valueOf(scanner.nextLine());
+        summa = number + summa;
+        sk+=1;
 
-if (number1 < 5000) {
-    System.out.println("No tax!");
-}
-else if (number1 >= 5000 && number1 < 25000) {
-    double tax1 = (100 + (number1-5000) * 0.08);
-    System.out.println("Tax: "+ tax1 );
-} 
-else if (number1 >= 25000 && number1 < 55000) {
-    double tax1 = (1700 + (number1-25000) * 0.10);
-    System.out.println("Tax: "+ tax1);
-} 
-else if (number1 >= 55000 && number1 < 200000) {
-    double tax1 = (4700 + (number1-55000) * 0.12);
-    System.out.println("Tax: "+ tax1);
-    }
-else if (number1 >= 200000 && number1 < 1000000) {
-    double tax1 = (22100 + (number1-200000) * 0.15);
-    System.out.println("Tax: "+ tax1);
-}
-else if (number1 >= 1000000) {
-    double tax1 = (142100 + (number1-1000000) * 0.17);
-    System.out.println("Tax: "+ tax1);
-}
+        if(number==0){
+            break;
+        }
+
+
+       }
+       System.out.println("Number of numbers: "+(sk-1));
+       System.out.println("Sum of the numbers: "+summa);
+
+       
     }
 }
