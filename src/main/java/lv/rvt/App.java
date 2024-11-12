d07ae65d61673a6197981eacf96f1b37c3f88429
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-11-12 08:23:31 +0000

    file reading

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -5,22 +5,14 @@ import java.io.BufferedWriter;
 import java.util.Scanner;
 public class App
 {
-public static void main(String[] args) {
-    PaymentCard paulsCard = new PaymentCard(20);
-    PaymentCard mattsCard = new PaymentCard(30);
-    paulsCard.eatHeartily();  
-    mattsCard.eatAffordably();
-    System.out.println("Paul: "+paulsCard);
-    System.out.println("Matt: "+mattsCard);
-    paulsCard.addMoney(20);
-    mattsCard.eatHeartily();
-    System.out.println("Paul: "+paulsCard);
-    System.out.println("Matt: "+mattsCard);
-    paulsCard.eatAffordably();
-    paulsCard.eatAffordably();
-    mattsCard.addMoney(50);
-    System.out.println("Paul: "+paulsCard);
-    System.out.println("Matt: "+mattsCard);
+public static void main(String[] args)  throws Exception {
+    BufferedReader reader = Utils.getReader("data.csv");
+    String line;
+    while((line = reader.readLine())!=null){
+        System.out.println(line);
+
+    }
+    
 
        
 }
