74446b21791520c59760bee319c7cb5301e6d955
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-11-19 10:15:41 +0000

    Constructor Overload

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,17 +10,12 @@ import org.w3c.dom.css.Counter;
 public class App
 {
 public static void main(String[] args)  throws Exception {
-    Timer timer = new Timer();
+    Product tapeMeasure = new Product("Tape measure");
+Product plaster = new Product("Plaster", "home improvement section");
+Product tyre = new Product("Tyre", 5);
 
-while (true) {
-    System.out.println(timer);
-    timer.advance();
-
-    try {
-        Thread.sleep(10);
-    } catch (Exception e) {
-
-    }
-}
+System.out.println(tapeMeasure);
+System.out.println(plaster);
+System.out.println(tyre);
 }
 }
--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -4,18 +4,56 @@ public class Product {
     private String name;
     private double price;
     private int quantity;
+    private int weight;
+    private String location;
     public Product (String initialName, double initialPrice, int initialQuantity){
         this.name = initialName;
         this.price = initialPrice;
         this.quantity = initialQuantity;
+        this.weight = weight;
+        this.location = location;
         
 
 
     }
-    public void printProduct(){
-        System.out.println(
-            this.name+ ", price "+ this.price+ ", "+this.quantity+" pcs"
-            );
+    public Product(String name){
+        this.name=name;
+        this.location="shelf";
+        this.weight=1;
+
+    }
+    public Product(String name, String location){
+        this.name=name;
+        this.location = location;
+        this.weight=1;
+
+
+    }
+    public Product(String name, int weight){
+        this.name=name;
+        this.weight=weight;
+        this.location="shelf";
+
+    }
+    public Product(String name, String location, int weight){
+        this.name=name;
+        this.weight=weight;
+        this.location=location;
+
+    }
+    public String getName(){
+        return name;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public String getlLocation(){
+        return location;
+    }
+    @Override
+    public  String toString(){
+        return  this.name+" ("+this.weight+" kg) can be found from the "+this.location;
+            
     }
     
 
