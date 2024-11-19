78b78a4520706c1ad84f237947fa9fdc09737ca9
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-11-19 10:09:33 +0000

    One Minute

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,18 +2,25 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+import org.w3c.dom.css.Counter;
 public class App
 {
 public static void main(String[] args)  throws Exception {
-    BufferedReader reader = Utils.getReader("data.csv");
-    String line;
-    while((line = reader.readLine())!=null){
-        System.out.println(line);
+    Timer timer = new Timer();
+
+while (true) {
+    System.out.println(timer);
+    timer.advance();
+
+    try {
+        Thread.sleep(10);
+    } catch (Exception e) {
 
     }
-    
-
-       
+}
 }
 }
