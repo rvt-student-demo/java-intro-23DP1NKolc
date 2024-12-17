519dc9d7ec07d342eb83101c48c7b49854fede56
23DP1NKolc <166384068+23DP1NKolc@users.noreply.github.com>
2024-12-17 08:44:46 +0000

    Card payments (4 sections)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -9,11 +9,25 @@ import java.util.Scanner;
 import org.w3c.dom.css.Counter;
 public class App
 {
-public static void main(String[] args)  throws Exception {
-    DecreasingCounter counter = new DecreasingCounter(1);
-    counter.increaseValue(2);
-    counter.increaseValue();
-    counter.increaseValue();
-    System.out.println(counter.getValue());
+public static void main(String[] args)  throws Exception {      
+PaymentTerminal unicafeExactum = new PaymentTerminal();
+    System.out.println(unicafeExactum);
+
+    PaymentCard annesCard = new PaymentCard(2);
+
+    System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
+
+    boolean wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+    System.out.println("there was enough money: " + wasSuccessful);
+
+    unicafeExactum.addMoneyToCard(annesCard, 100);
+
+    wasSuccessful = unicafeExactum.eatHeartily(annesCard);
+    System.out.println("there was enough money: " + wasSuccessful);
+
+    System.out.println("amount of money on the card is " + annesCard.balance() + " euros");
+
+    System.out.println(unicafeExactum);
+    
 }
 }
--- src/main/java/lv/rvt/PaymentCard.java
+++ src/main/java/lv/rvt/PaymentCard.java
@@ -3,28 +3,26 @@ package lv.rvt;
 public class PaymentCard {
     private double balance;
 
-    public PaymentCard(double openingBalance) {
-        this.balance=openingBalance;
+    public PaymentCard(double balance) {
+        this.balance = balance;
     }
 
-    public String toString() {
-        return "The card has a balance of "+balance+" euros";
-    }
-    public void eatAffordably() {
-        if (this.balance >=2.60){
-            this.balance -= 2.60;
-        }
-    }
-    
-    public void eatHeartily() {
-        if (this.balance >=4.60){
-            this.balance -= 4.60;
-        }
+    public double balance() {
+        return this.balance;
     }
     public void addMoney(double amount) {
         if(amount>=0){
         this.balance+=amount;
         }
+    }
+    public boolean takeMoney(double amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    
+        
     }
 
 }
